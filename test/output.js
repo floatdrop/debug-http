e30d778fd1be37678b1913f4b7b32387fdc93777
Vsevolod Strukchinsky <floatdrop@gmail.com>
2015-12-12 17:05:04 +0500

    use local server for output tests

--- test/output.js
+++ test/output.js
@@ -2,22 +2,35 @@ import test from 'ava';
 import http from 'http';
 import debugHttp from '../';
 
+let server;
+
 test('nice stuff', t => {
+	server = http.createServer(function (rq, res) {
+		res.end('OK');
+	}).listen(8081);
+
 	debugHttp();
 
 	let i = 0;
 	console.log = function (string) {
 		if (i === 0) {
-			t.is(string, '      → GET http://google.com/');
+			t.is(string, '      → GET http://localhost:8081/');
 		}
 
 		if (i === 1) {
-			t.regexTest(/302 ← GET http:\/\/google.com\/ (\d+)ms/, string);
+			t.regexTest(/200 ← GET http:\/\/localhost:8081\/ (\d+)ms/, string);
 			t.end();
 		}
 
 		i++;
 	};
 
-	http.get('http://google.com');
+	http.get('http://localhost:8081');
+});
+
+test.after('teardown', t => {
+	if (server) {
+		server.close();
+		t.end();
+	}
 });
